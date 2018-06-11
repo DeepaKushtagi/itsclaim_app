86c1b9f90afca74fededcb3913751d64225aa700
DeepaKushtagi <36659189+DeepaKushtagi@users.noreply.github.com>
2018-06-11 10:39:30 +0000

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      //logger.log("Hello, World");
-       System.out.println("Hello, World");
-   }
-}
