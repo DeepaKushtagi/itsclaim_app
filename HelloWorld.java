0abe2adef49286238b076748affc74397f931c14
DeepaKushtagi <36659189+DeepaKushtagi@users.noreply.github.com>
2018-06-11 15:13:23 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      //logger.log("Hello, World");
+       System.out.println("Hello, World");
+   }
+}
